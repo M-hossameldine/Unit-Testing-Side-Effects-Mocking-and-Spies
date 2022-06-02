0fee401b18608d18aeecc50f11ead42a9bc57d49
Mohamed Hossam <m.hossameldine@gmail.com>
2022-06-02 19:02:38 +0200

    docs: add logFn comment

--- src/data.js
+++ src/data.js
@@ -2,6 +2,8 @@ import writeData from './util/io.js';
 
 export function generateReportData(logFn) {
   const data = 'Some dummy data for this demo app';
+
+  // logFn() this is a side effect we just want to find out whether the logFn is called when provided
   if (logFn) {
     logFn(data);
   }
